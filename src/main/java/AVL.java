21cce9f09957c96107748a1628c76da46d0624a4
Ramy <ramyfetteha@gmail.com>
2022-03-09 18:57:52 +0200

    .

---
+++ src/main/java/AVL.java
@@ -0,0 +1,250 @@
+import java.util.Objects;
+
+import static java.lang.Math.*;
+
+class Node{
+    String value;
+    int height;
+
+    Node left, right;
+
+
+    Node(String number)
+    {
+        value = number;
+        height = 1;
+    }
+
+}
+
+
+class AVL {
+    Node root;
+    int size = 0;
+
+    int getHeight(Node a)
+    {
+        if (a == null)
+        {
+            return 0;
+        }
+        return a.height;
+    }
+
+    Node rightRotation(Node current)
+    {
+        Node nRoot = current.left;
+        current.left = nRoot.right;
+        nRoot.right = current;
+
+        current.height = max(getHeight(current.left), getHeight(current.right)) + 1;
+        nRoot.height =  max(getHeight(nRoot.left), getHeight(nRoot.right)) + 1;
+
+
+        return nRoot;
+    }
+
+    Node leftRotation(Node current)
+    {
+
+        Node nRoot = current.right;
+
+        current.right = nRoot.left;
+
+
+        nRoot.left = current;
+
+        current.height = max(getHeight(current.left), getHeight(current.right)) + 1;
+        nRoot.height =  max(getHeight(nRoot.left), getHeight(nRoot.right)) + 1;
+
+
+        return nRoot;
+    }
+
+    Node insert(Node node, String value)
+    {
+        if (node == null)
+        {
+            return new Node(value);
+        }
+
+        if (value.compareTo(node.value) < 0)
+        {
+            node.left = insert(node.left, value);
+        }
+        else if(value.compareTo(node.value) > 0)
+        {
+            node.right = insert(node.right, value);
+        }
+        else
+            return node;
+
+        node.height = max(getHeight(node.right), getHeight(node.left)) + 1;
+
+        int balanceFactor = balanceFactor(node);
+
+        if (balanceFactor > 1 && value.compareTo(node.left.value) < 0)
+        {
+            return rightRotation(node);
+        }
+        if(balanceFactor < -1 && value.compareTo(node.right.value) > 0)
+        {
+            return leftRotation(node);
+        }
+        if(balanceFactor > 1 && value.compareTo(node.left.value) > 0)
+        {
+            node.left = leftRotation(node.left);
+            return rightRotation(node);
+        }
+        if(balanceFactor < -1 && value.compareTo(node.right.value) < 0)
+        {
+            node.right = rightRotation(node.right);
+            return leftRotation(node);
+        }
+
+
+        return node;
+    }
+
+    int search(Node root, String value)
+    {
+        if (root == null)
+        {
+            System.out.println("NO");
+            return 0;
+        }
+
+        if (value.compareTo(root.value) == 0)
+        {
+            System.out.println("YES");
+            return 1;
+        }
+        else if(value.compareTo(root.value) < 0)
+        {
+            return search(root.left, value);
+        }
+        else if(value.compareTo(root.value) > 0)
+        {
+            return search(root.right,value);
+        }
+
+        return 0;
+    }
+
+    void getSize(Node root)
+    {
+        if (root != null)
+        {
+            this.size += 1;
+            getSize(root.left);
+            getSize(root.right);
+        }
+    }
+
+    int balanceFactor(Node node)
+    {
+        if (node == null)
+            return 0;
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    Node delete(Node root, String value)
+    {
+        if(root == null)
+        {
+            return root;
+        }
+
+        if (value.compareTo(root.value) < 0)
+        {
+            root.left = delete(root.left, value);
+        }
+        else if(value.compareTo(root.value) > 0)
+        {
+            root.right = delete(root.right, value);
+        }
+        else
+        {
+            if (root.left == null || root.right == null)
+            {
+                Node temp = null;
+                if (temp == root.left)
+                    temp = root.right;
+                else
+                    temp = root.left;
+
+                if (temp == null)
+                {
+                    temp = root;
+                    root = null;
+                }
+                else
+                    root = temp;
+            }
+            else
+            {
+                Node temp = lowestNode(root.right);
+                root.value = temp.value;
+                root.right = delete(root.right, temp.value);
+            }
+        }
+
+        if (root == null)
+        {
+            return root;
+        }
+
+        root.height = max(getHeight(root.right), getHeight(root.left)) + 1;
+
+        int balanceFactor = balanceFactor(root);
+
+        if (balanceFactor > 1 && balanceFactor(root.left) >= 0)
+        {
+            return rightRotation(root);
+        }
+        if(balanceFactor < -1 && balanceFactor(root.left) < 0)
+        {
+            return leftRotation(root);
+        }
+        if(balanceFactor > 1 && balanceFactor(root.right) <= 0)
+        {
+            root.left = leftRotation(root.left);
+            return rightRotation(root);
+        }
+        if(balanceFactor < -1 && balanceFactor(root.right) > 0)
+        {
+            root.right = rightRotation(root.right);
+            return leftRotation(root);
+        }
+
+        return root;
+    }
+
+
+    Node lowestNode(Node root)
+    {
+        Node temp = root;
+        while (root.left != null)
+        {
+            temp = root.left;
+        }
+
+        return temp;
+
+    }
+
+    public static void main(String[] args)
+    {
+        AVL tree = new AVL();
+
+        tree.root = tree.insert(tree.root, "10");
+        tree.root = tree.insert(tree.root, "20");
+        tree.root = tree.insert(tree.root, "30");
+        tree.root = tree.insert(tree.root, "40");
+        tree.root = tree.insert(tree.root, "50");
+        tree.root = tree.insert(tree.root, "25");
+        int yes = tree.search(tree.root, "5");
+        tree.root = tree.delete(tree.root, "30");
+        System.out.println(tree.getHeight(tree.root));
+    }
+}
---
+++ src/main/java/Dictionary.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Dictionary {
+    public static void main(String[] args) throws IOException {
+        AVL dictionary = new AVL();
+
+        System.out.println("1 - Load dictionary");
+        System.out.println("2 - Get dictionary size");
+        System.out.println("3 - Insert a word");
+        System.out.println("4 - Search for a word");
+        System.out.println("5 - Remove a word");
+        System.out.println("6 - Batch look-up");
+        System.out.println("7 - Batch remove");
+        System.out.println("Please insert the number of the command required!");
+        while (true)
+        {
+
+            Scanner scanner = new Scanner(System.in);
+            String option = scanner.nextLine();
+            String word;
+            switch (option)
+            {
+                case "1":
+                    BufferedReader reader = new BufferedReader(new FileReader("src/main/java/dictionary.txt"));
+                    while ((word = reader.readLine()) != null) {
+                         dictionary.root = dictionary.insert(dictionary.root, word);
+                    }
+                    reader.close();
+                    System.out.println("Dictionary Loaded!");
+                    break;
+                case "2":
+                    dictionary.size = 0;
+                    dictionary.getSize(dictionary.root);
+                    System.out.println("Size: " + dictionary.size);
+                    break;
+                case "3":
+                    word = scanner.nextLine();
+                    dictionary.root = dictionary.insert(dictionary.root, word);
+                    System.out.println("Insertion done!");
+                    break;
+                case "4":
+                    word = scanner.nextLine();
+                    dictionary.search(dictionary.root, word);
+                    break;
+                case "5":
+                    word = scanner.nextLine();
+                    dictionary.root = dictionary.delete(dictionary.root, word);
+                    System.out.println("Deletion done!");
+                    break;
+                case "6":
+                    int found = 0;
+                    int check = 0;
+                    reader = new BufferedReader(new FileReader("src/main/java/queries.txt"));
+                    while ((word = reader.readLine()) != null) {
+                        check = dictionary.search(dictionary.root, word);
+                        if (check == 1)
+                            found += 1;
+                    }
+                    System.out.println("Entries found: " + found);
+                    reader.close();
+                    break;
+                case "7":
+                    reader = new BufferedReader(new FileReader("src/main/java/deletions.txt"));
+                    while ((word = reader.readLine()) != null) {
+                        dictionary.root = dictionary.delete(dictionary.root, word);
+                    }
+                    reader.close();
+                    System.out.println("Batch deletion done!");
+                    break;
+
+                default:
+                    System.out.println("Invalid");
+                    break;
+
+            }
+        }
+    }
+}
